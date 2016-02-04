e02c0817a2958bb4c41970f5887ee1c7a39c4cd6
Guoping Tang <tanggp@yahoo.com>
2016-02-04 14:38:54 -0500

    Output absolute value of reaction fluxes for iMG746 case0.

--- cobrapy/img746/case0.py
+++ cobrapy/img746/case0.py
@@ -13,6 +13,15 @@ cobra.io.save_json_model(model, "iMG746.json")
 with open('case0rxn.json', 'w') as f:
   json.dump(model.solution.x_dict, f)
 
+rxnrates = model.solution.x_dict
+
+for rxn in rxnrates:
+  if rxnrates[rxn] < 0.0:
+    rxnrates[rxn] = abs(rxnrates[rxn])
+
+with open('case0rxnabs.json', 'w') as f:
+  json.dump(rxnrates, f)
+
 ex_rxns = model.reactions.query('EX')
 for rxn in ex_rxns:
   if abs(rxn.x) > 1.0e-10:
